e385ad1c4b12f2c574c61b3540dffebb4789071c
fmascare <fem0022@gmail.com>
2021-10-10 19:35:05 -0500

    Initial author routes

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+//const bodyParser = require('body-parser')
 
-const indexRouter = require('./routes/index')
-
+app.use(express.urlencoded({ limit: '10mb', extended: false }))
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
-app.use(express.static('public'))
-app.use('/', indexRouter)
 
+//Routing Info
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
+app.use('/', indexRouter)
+app.use('/authors',authorRouter)
+
+//Database Info
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', error => console.error(error))
